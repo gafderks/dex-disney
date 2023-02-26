5e74ac7517e62faed17e76035c4199923de44172
Geert <geertderks12@gmail.com>
2023-02-27 00:09:34 +0100

    User select none and blurry text

--- src/components/CollectableItem.vue
+++ src/components/CollectableItem.vue
@@ -15,21 +15,38 @@ const fullUrl = computed(() => {
 </script>
 <template>
     <div class="col">
-    <div class="card" style="width: 18rem;">
-        <img :src="fullUrl" width="200" :class="[{'found': value.found}, 'card-img-top']" />
-        <div class="card-body">
-            <h5 class="card-title">{{ value.name }}</h5>
+        <div :class="['card', { 'found': value.found }]" style="width: 18rem;">
+            <img :src="fullUrl" width="200" class="card-img-top" />
+            <div class="card-body">
+                <h5 class="card-title">{{ value.name }}</h5>
+            </div>
         </div>
     </div>
-</div>
 </template>
 
 <style lang="scss" scoped>
-img {
-    filter:brightness(0.05);
+.card {
+    img {
+        filter: brightness(0.05);
+        user-select: none;
+        pointer-events: none;
+    }
+
+    .card-title {
+        text-shadow: 0 0 10px var(--bs-body-color);
+        color: transparent;
+        user-select: none;
+    }
 
     &.found {
-        filter: none;
+        img {
+            filter: none;
+        }
+
+        .card-title {
+            text-shadow: none;
+            color: var(--bs-body-color);
+        }
     }
 }
 </style>
